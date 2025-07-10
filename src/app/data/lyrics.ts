0b2590c53cc98bff89aedbe363c2fc3d0d1e9127
Valli-Arnason <thorvaldur.arnason@gmail.com>
2025-07-10 14:38:04 +0000

    Inital prod commit

---
+++ src/app/data/lyrics.ts
@@ -0,0 +1,490 @@
+export type Lyric = {
+  id: string
+  title: string
+  artist: string
+  lyrics: string
+}
+
+export const lyricsData: Lyric[] = [
+
+   {
+    id: 'song1',
+    title: 'Ó María mig langar heim',
+    artist: 'Artist C',
+    lyrics: ` Hann sigldi út um höfin blá í 17 ár
+ og sjómennsku kunni hann upp á hár,
+ Hann saknaði alla tíð stúlkunnar
+ og mynd hennar stöðugt í hjarta hann bar.
+ 
+ Ó, María mig langar heim.
+ Ó, María mig langar heim.
+ Því heima vil ég helst vera.
+ Ó, María hjá þér.
+ 
+ Í höfnum var hann hrókur mikils fagnaðar
+ og heillaði allstaðar stúlkurnar
+ en meyjarnar aldrei í augun leit
+ það átti ekki við hann að rjúfa sín heit.
+ 
+ Ó, María mig langar heim.
+ Ó, María mig langar heim.
+ Því heima vil ég helst vera.
+ Ó, María hjá þér.
+ 
+ Svo kom að því, hann vildi halda heim á leið
+ til hennar sem sat þar og beið og beið
+ hann hætti til sjós, tók sinn hatt og staf
+ og heimleiðis sigldi um ólgandi haf.
+ 
+ Ó, María mig langar heim.
+ Ó, María mig langar heim.
+ Því heima vil ég helst vera.
+ Ó, María hjá þér.
+ 
+ En fleyið bar hann aldrei heim að fjarðarströnd.
+ Hann ferðast ei lengur um ókunn lönd.
+ En María bíður og bíður enn
+ Hún bíður og vonar hann komi nú senn.
+ 
+ Ó, María mig langar heim.
+ Ó, María mig langar heim.
+ Því heima vil ég helst vera.
+ Ó, María hjá þér.
+ 
+ Ó, María mig langar heim.
+ Ó, María mig langar heim.
+ Því heima vil ég helst vera.
+ Ó, María hjá þér.
+ Ó, María hjá þér.
+ Ó, María hjá þér.`,
+  },
+
+    {
+    id: 'song2',
+    title: 'Minning um mann',
+    artist: 'Artist C',
+    lyrics: ` Nú ætla ég að syngja ykkur lítið fallegt ljóð
+ um ljúfan dreng, sem fallinn er nú frá,
+ um dreng sem átti sorgir en ávallt samt þó stóð
+ sperrtur þó að sitthvað gengi á.
+
+ Í kofaskrifli bjó hann, sem lítinn veitti yl,
+ svo andvaka á nóttum oft hann lá.
+ Þá Portúgal hann teygaði, það gerði ekkert til,
+ það tókst með honum yl í sig að fá.
+
+ Þið þekktuð þennan mann,
+ þið alloft sáuð hann,
+ drykkjuskap til frægðar sér hann vann.
+ 
+ Börnum var hann góður, en sum þó hræddust hann,
+ hæddu hann og gerðu að honum gys.
+ Þau þekktu ei litlu greyin þennan mæta mann,
+ já, margt er það sem börnin fara á mis.
+ 
+ Þið þekktuð þennan mann,
+ þið alloft sáuð hann,
+ drykkjuskap til frægðar sér hann vann.
+
+ Munið þið að dæma ei eftir útlitinu menn,
+ en ýmsum yfir þessa hluti sést.
+ Því til er það að flagð er undir fögru skinni enn
+ en fegurðin að innan þykir best.
+
+ Þið þekktuð þennan mann,
+ þið alloft sáuð hann,
+ drykkjuskap til frægðar sér hann vann.
+
+ Nú ljóðið er á enda um þennan sómasvein,
+ sem að þráði brennivín og sæ.
+ Hann liggur nú á kistubotni og lúin hvílir bein,
+ í kirkjugarði í Vestmannaeyjabæ.
+ 
+ Þið þekktuð þennan mann,
+ þið alloft sáuð hann,
+ drykkjuskap til frægðar sér hann vann.`,
+  },
+
+ {
+    id: 'song3',
+    title: 'Óbyggðirnar kalla',
+    artist: 'Artist C',
+    lyrics: ` Hoppa kátur út um dyrnar
+ við blasir heimurinn.
+ Himinblár er bláminn.
+ Himneskur jökullinn. 
+
+ Óbyggðirnar kalla og ég verð að gegna þeim.
+ Ég veit ekki hvort eða hvernig
+ eða hvenær ég kemst heim.
+ Ég veit ekki hvort eða        	  
+ hvernig eða hvenær
+ ég kemst heim.
+
+ Bergmál óbyggðanna
+ svo bjart í höfði mér.
+ Leiður á öllu og öllum
+ hundleiður á sjálfum mér.
+
+ Óbyggðirnar kalla og ég verð að gegna þeim.
+ Ég veit ekki hvort eða hvernig
+ eða hvenær ég kemst heim.
+ Ég veit ekki hvort eða        	  
+ hvernig eða hvenær
+ ég kemst heim.
+
+ Hoppa kátur út um gluggann
+ úr blokk á fyrstu hæð.
+ Svo siglir sálarduggan
+ í allri sinni smæð.
+
+ Óbyggðirnar kalla og ég verð að gegna þeim.
+ Ég veit ekki hvort eða hvernig
+ eða hvenær ég kemst heim.
+ Ég veit ekki hvort eða        	  
+ hvernig eða hvenær
+ ég kemst heim.`,
+  },  
+
+{
+    id: 'song4',
+    title: 'Undir bláhimni',
+    artist: '',
+    lyrics: `Undir bláhimni blíðsumars nætur
+barstu í arma mér rósfagra mey
+Þar sem döggin í grasinu grætur,
+gárast tjörnin í suðrænum þey.
+
+Ég var snortinn af yndisleik þínum
+ástarþráin er vonunum felld.
+Þú ert ljósblik á lífshimni mínum
+þú ert ljóð mitt og stjarna í kveld.
+
+Ég vil dansa við þig, meðan dunar
+þetta draumblíða lag, sem ég ann,
+Meðan fjörið í æðunum funar,
+og af fögnuði hjarta mitt brann.
+
+Og svo dönsum við dátt, þá er gaman,
+meðan dagur í austrinu rís.
+Og svo leiðumst við syngjandi saman,
+út í sumarsins paradís.`,
+  },
+
+{
+    id: 'song5',
+    title: 'Traustur vinur',
+    artist: '',
+    lyrics: ` Enginn veit fyrr en reynir á
+hvort vini áttu þá.
+
+Fyrirheit gleymast þá furðu fljótt
+þegar fellur á niðdimm nótt.
+Já sagt er að, þegar af könnunni ölið þverr
+fljótt þá vinurinn fer.
+
+Því segi ég það, ef þú átt vin í raun
+fyrir þína hönd Guði sé laun.
+
+Því stundum verður mönnum á
+styrka hönd þeir þurfa þá
+þegar lífið, allt í einu
+sýnist einskisvert.
+
+Gott er að geta talað við
+einhvern sem að skilur þig.
+Traustur vinur getur gert
+kraftaverk.     
+
+Mér varð á, og þungan dóm ég hlaut
+ég villtist af réttri braut.
+Því segi ég það, ef þú átt vin í raun.
+Fyrir þína hönd Guði sé laun.
+
+Því stundum verður mönnum á
+styrka hönd þeir þurfa þá
+þegar lífið, allt í einu
+sýnist einskisvert.
+
+Gott er að geta talað við
+einhvern sem að skilur þig.
+Traustur vinur getur gert
+kraftaverk.`,
+  },
+
+{
+    id: 'song6',
+    title: 'Litla flugan',
+    artist: '',
+    lyrics: `Lækur tifar létt um máða steina.
+Lítil fjóla grær við skriðufót
+Bláskel liggur brotin milli hleina.
+Í bænum hvílir íturvaxin snót.    
+
+Ef ég væri orðinn lítil fluga,
+Ég inn um gluggann þreytti flugið mitt,                           	
+og þó ég ei til annars mætti duga,
+ég  eflaust gæti kitlað nefið þitt     	 
+ég eflaust gæti kitlað nefið þitt.`,
+  },  
+
+  {
+    id: 'song7',
+    title: 'Vor í Vaglaskógi',
+    artist: '',
+    lyrics: `Kvöldið er okkar og vor um Vaglaskóg,
+við skulum tjalda í grænum berjamó.
+Leiddu mig vinur í lundinn frá í gær,
+lindin þar niðar og birkihríslan grær.
+
+Leikur í ljósum lokkum og angandi rósum,
+leikur í ljósum lokkum hinn vaggandi blær.
+
+Daggperlur glitra um dalinn færist ró,
+draumar þess rætast sem gistir Vaglaskóg.
+Kveldrauðu skini á krækilyngið slær.
+Kyrrðin er friðandi, mild og angurvær.
+
+Leikur í ljósum lokkum og angandi rósum,
+leikur í ljósum lokkum hinn vaggandi blær.
+`,
+  },
+
+  {
+    id: 'song8',
+    title: 'Diggy liggi læ',
+    artist: '',
+    lyrics: `"Diggy liggy læla, diggy liggy ló"
+sagði einn drengur sem hérna bjó 
+Er sorg var í húsi hann sagði'og hló:
+"Diggy liggy læla diggy, diggy liggy ló."
+
+"Diggy liggy læla, diggy liggy ló"
+Allir skyldu hann en hváðu þó.
+Þeim var ljóst hann þekkti' ei nóg
+af þessum heimi, diggy liggy ló.
+
+Daggperlur glitra um dalinn færist ró,
+draumar þess rætast sem gistir Vaglaskóg.
+Kveldrauðu skini á krækilyngið slær.
+Kyrrðin er friðandi, mild og angurvær.
+
+Allir vita'að ástand hér
+ekki neitt til sóma er 
+Orðin týnast ótt af því
+oft er þörf að gera ný
+
+"Diggy liggy læla diggy, liggy ló"
+Allir skyldu hann en hváðu þó.
+Þeim var ljóst hann þekkti' ei nóg
+af þessum heimi, diggy liggy ló.
+
+Allir vita'að ástand hér
+ekki neitt til sóma er 
+Orðin týnast ótt af því
+oft er þörf að gera ný 
+
+"Diggy liggy læla diggy, liggy ló"
+Allir skyldu hann en hváðu þó.
+Þeim var ljóst hann þekkti' ei nóg
+af þessum heimi, diggy liggy ló. 
+ 
+"Diggy liggy læla, diggy liggy ló" osfr.`,
+  },
+
+  {
+    id: 'song9',
+    title: 'Farin',
+    artist: '',
+    lyrics: `Ertu þá farin
+ertu þá farin frá mér?
+Hvar ertu núna,
+hvert liggur mín leið?
+
+Spyrjum hvort annað
+hvort fari ég einn í nótt
+
+Það er liðinn langur tími
+og ég valdi þessi orð
+Skrítið hvað tíminn fer þér vel
+Nóttin siglir að, minningin er kyrr
+kemur og stimplar sig inn
+
+Ertu þá farin
+ertu þá farin frá mér?
+Hvar ertu núna,
+hvert liggur mín leið?
+
+Spyrjum hvort annað
+hvort fari ég einn í nótt
+
+Tíminn líður, líður án mín
+en þú kemur ekki í kvöld
+ég bað þig svo lengi
+að vera aðeins hér
+og að þú standir mér hjá
+
+Ertu þá farin
+ertu þá farin frá mér?
+Hvar ertu núna,
+hvert liggur mín leið?
+
+Spyrjum hvort annað
+hvort fari ég einn í nótt
+
+Í sýn ömurleg, birtist mynd af þér
+
+Sé ég alla leið   ohhh…
+
+Ef ég hefði boðið betur
+og verið þér nær
+hlustað og gefið af mér
+Það þýðir ei lengur að gefa þig upp
+þegar ég hringdi, ekkert svar
+
+Ertu þá farin
+ertu þá farin frá mér?
+Hvar ertu núna,
+hvert liggur mín leið?
+
+Spyrjum hvort annað
+hvort fari ég einn í nótt`,
+  },
+
+  {
+    id: 'song10',
+    title: 'Fram á nótt',
+    artist: '',
+    lyrics: `Börn, og aðrir minna þroskaðir menn
+fóru að gramsa í mínum einkamálum
+þegar ég var óharðnaður enn
+og átti erfitt með að miðla málum.
+
+Þú varst að ganga rekin í kút
+til þess að verða fyrir aðkasti mannana
+þó að þú lítir alls ekki út
+fyrir að lifa eftir lögum þess bannaða.
+
+En hvernig kemst ég inn þegar allt er orðið hljótt
+þá verða menn um sinn að djamma fram á nótt.
+En hvernig kemst ég inn þegar allt er orðið hljótt
+þá verða menn um sinn að djamma.
+
+Mitt, vandamál er á andlega sviðinu
+hugsanirnar heimskar sem gínur á húsþökum
+þú ættir að sjá í andlitið á liðinu
+er það sér úr þessu vandræði við bökum
+
+Þú varst að ganga rekin í ...`,
+  },
+  
+  {
+    id: 'song11',
+    title: 'Með stjörnunum',
+    artist: '',
+    lyrics: `Hey ertu ein eftir erfiðan dag
+því að ég man að þú skrifaðir allt niður á blað
+Nú ertu einmana
+Af hverju einmana.
+ 
+Þú sýndir mér margt sem að ég hef ei gleymt
+Nú hlýtur einhver annar að vera með þér
+Nú er ég einmana	
+Af hverju einmana?
+ 
+Því ég  	....anda þér
+ 
+Það er erfitt að sjá þig.
+Það er erfitt að ná í mig
+Já út í glugganum
+Þú og ég með stjörnunum.
+ 
+Það er erfitt að sjá þig.
+Það er erfitt að ná í mig
+Já út í glugganum
+Þú og ég með stjörnunum.
+ 
+Man ég þá stund þegar þú varst mér hjá
+Það var engin önnur kona nálægt mér þá
+Nú er ég einmana
+Af hverju einmana?
+ 
+Þú sýndir mér margt sem að ég hef ei gleymt
+Nú hlýtur meinhver annar að vera með þér
+Nú er ég einmana 
+Af hverju einmana?
+ 
+Því ég . 	....anda þér  
+ 
+Það er erfitt að sjá þig.
+Það er erfitt að ná í mig
+Já út í glugganum
+Þú og ég með stjörnunum.
+ 
+Oh óuu,, Aldrei einn, aldrei einn
+Því að stundum líður mér
+Eins og þú sért jafnvel hjá mér
+Aldrei einn
+Þú og ég með stjörnunum.
+ 
+Það er erfitt að sjá þig.
+Það er erfitt að ná í mig
+Já út í glugganum
+Þú og ég með stjörnunum.
+ 
+Það er erfitt að sjá þig.
+Það er erfitt að ná í mig
+Já út í glugganum
+Þú og ég með stjörnunum. - Ohhhh
+ 
+Oh, oh oh oh oh - oh oh oh oh - Oh, ohh
+Oh, oh oh oh oh - oh oh oh oh - Oh, ohh
+Oh, oh oh oh oh - oh oh oh oh - Oh, ohh
+(Þú og ég með stjörnunum)
+Oh, oh oh oh oh - oh oh oh oh - Oh, ohh`,
+  },  
+
+  {
+    id: 'song12',
+    title: 'Blátt lítið blóm eitt er',
+    artist: '',
+    lyrics: `Blátt lítið blóm eitt er,
+ber nafnið; gleymd ei mér.
+Væri ég fleygur fugl,
+flygi ég til þín.
+
+Svo mína sálu nú
+sigraða hefur þú,
+engu ég unna má
+öðru en þér.
+
+Leggjum svo kinn við kinn,
+komdu með faðminn þinn.
+Hátt yfir hálsinn minn,
+hönd þína breið.
+Svo mína sálu nú
+sigraða hefur þú,
+engu ég unna má
+öðru en þér.`,
+  },
+
+  {
+    id: 'song13',
+    title: 'Bæjarvísur (eftir laginu Yfir kaldan eyðisand)',
+    artist: '',
+    lyrics: `Gröf og Ásar glöggt ég les,
+glaður sést þar halur,
+Hemra, Flaga, Hrífunes,
+Hlíð og Svínadalur.
+
+Fljótastaðir fá oft skell,
+fást í Seli rjúpur,
+Búland, Hvammur, Borgarfell,
+býli snæs og Núpur.
+
+Sól á húsum sindrar blíð
+sjá má bros á lýði 
+Múli Út og Austurhlíð 
+eru sveitarprýði`,
+  },
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,117 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { useEffect, useState } from 'react'
+import { lyricsData } from './data/lyrics'
+
+const STORAGE_KEY = 'lyrics-expanded-state'
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [openSections, setOpenSections] = useState<Record<string, boolean>>({})
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  useEffect(() => {
+    const saved = localStorage.getItem(STORAGE_KEY)
+    if (saved) {
+      try {
+        setOpenSections(JSON.parse(saved))
+      } catch {
+        setOpenSections({})
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(openSections))
+  }, [openSections])
+
+  const toggleSection = (id: string) => {
+    setOpenSections((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }))
+  }
+
+  return (
+    <main style={{ fontFamily: 'sans-serif' }}>
+      {/* Hero section */}
+      <div
+        style={{
+          position: 'relative',
+          height: '300px',
+          backgroundImage: 'url(/hero.jpg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          textShadow: '0 0 8px rgba(0,0,0,0.7)',
+        }}
+      >
+        <h1
+          style={{
+            fontSize: '2.5rem',
+            textAlign: 'center',
+            padding: '0 1rem',
+            maxWidth: '90%',
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+          Ættarmót í Tunguseli 2025
+        </h1>
+      </div>
+
+      {/* Lyrics list */}
+      <section style={{ padding: '2rem' }}>
+        {lyricsData.map(({ id, title, lyrics }) => {
+          const isOpen = openSections[id]
+
+          return (
+            <section
+              key={id}
+              style={{
+                border: '1px solid #ccc',
+                borderRadius: '6px',
+                marginBottom: '1rem',
+                padding: '1rem',
+                transition: 'all 0.3s ease',
+              }}
+            >
+              <button
+                onClick={() => toggleSection(id)}
+                style={{
+                  all: 'unset',
+                  cursor: 'pointer',
+                  fontSize: '1.2rem',
+                  fontWeight: 'bold',
+                  color: '#0070f3',
+                }}
+                aria-expanded={isOpen}
+                aria-controls={`lyrics${id}`}
+              >
+                {isOpen ? '▼' : '▶'} {title}
+              </button>
+
+              <div
+                id={`lyrics${id}`}
+                style={{
+                  maxHeight: isOpen ? '1000px' : '0',
+                  opacity: isOpen ? 1 : 0,
+                  overflow: 'hidden',
+                  transition: 'all 0.4s ease',
+                }}
+              >
+                <pre
+                  style={{
+                    whiteSpace: 'pre-wrap',
+                    marginTop: '1rem',
+                    lineHeight: 1.5,
+                  }}
+                >
+                  {lyrics}
+                </pre>
+              </div>
+            </section>
+          )
+        })}
+      </section>
+    </main>
+  )
 }
